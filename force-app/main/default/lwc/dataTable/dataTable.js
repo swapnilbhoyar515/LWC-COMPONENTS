461babe7a2a1b0069496b56929bc2c8a5946ce6d
Swapnil Bhoyar <swapnilbhoyar515@gmail.com>
2023-11-21 10:03:15 +0530

    newChangesLMS

--- force-app/main/default/lwc/dataTable/dataTable.js
+++ force-app/main/default/lwc/dataTable/dataTable.js
@@ -12,15 +12,26 @@ export default class DataTable extends LightningElement {
     columns =columns;
     data=[];
     error;
+
+
+
+    connectedCallback(){
+      console.log('connected Call back');
+    }
+
    
     @wire(getAccountRecords)
-
     getAccountFunction({data,error}){
+      console.log('wire');
       if(data){
              this.data = data;
+             console.log('wire 1');
       }
       else if(error){
        this.error=error;
       }
     }
+
+    
+    
 }
